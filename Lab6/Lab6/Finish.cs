be410d6d236682a2f2c8ca3b5f70df1eba578777
suvorovnk <62286367+suvorovnk@users.noreply.github.com>
2020-05-04 16:41:42 +0300

    Лабораторная работа 6

---
+++ Lab6/Lab6/Finish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Finish : Form
+    {
+        public Finish()
+        {
+            InitializeComponent();
+        }
+        User us=new User();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            us.Change(textBox1.Text);
+            MessageBox.Show("Пароль успешно изменён");
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.UseSystemPasswordChar = checkBox1.Checked;
+            textBox1.PasswordChar = '*';
+        }
+    }
+}
---
+++ Lab6/Lab6/Start.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Start : Form
+    {
+        User us=new User();
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (us.Check(textBox1.Text, textBox2.Text)!="Error")
+            {
+                this.Hide();
+                Finish f = new Finish();
+                f.Text = us.Check(textBox1.Text, textBox2.Text);
+                f.Show();
+            }
+            else MessageBox.Show("Логин и пароль введены неверно");
+        }
+    }
+}
